971a77cf476479f8c176dd43475d74ff82736d66
wanggonghe <wanggonghe@cestc.cn>
2021-03-29 10:44:19 +0800

    add(views):添加demo1

--- src/views/demo1.vue
+++ src/views/demo1.vue
@@ -1,5 +1,31 @@
 <template>
-  <div>
-    demo1
-  </div>
+  <div ref="pixiRef"></div>
 </template>
+
+<script>
+import * as PIXI from 'pixi.js'
+
+export default {
+  mounted() {
+    this.init()
+  },
+  methods: {
+    /**
+     * 初始化
+     */
+    init() {
+      const app = new PIXI.Application({
+        width: 800, // default 800
+        height: 600, // default 600
+        transparent: false, // default false
+        anitialias: true, // default true
+        resolution: 1 // default 1
+      });
+
+      const { $refs: { pixiRef } } = this
+      pixiRef.appendChild(app.view)
+
+    }
+  }
+}
+</script>
